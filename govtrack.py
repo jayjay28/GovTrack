d934caaf3caad81bec0e8fad63f905218476d847
clyon jackson <JayJay@clyons-MacBook-Pro.local>
2016-09-15 15:29:02 -0400

    update and added search bill function to the Govtrack Class

--- govtrack.py
+++ govtrack.py
@@ -6,45 +6,72 @@ class GovTrack():
 	import pprint
 	import urllib2
 	import json
+	import pprint
 
 	def __init__(self):
-		a = "Barry Goldwater"
-		f = GovTrack.urllib2.urlopen('https://www.govtrack.us/api/v2/person?q=' + a.replace(" ","%20"))
+		self.a ="Hillary Clinton"
+		f = GovTrack.urllib2.urlopen('https://www.govtrack.us/api/v2/person?q=' + self.a.replace(" ","%20"))
 		json_string = f.read()
 		self.parsed_json = GovTrack.json.loads(json_string)
 		f.close()
-
 		self.congress_members = {}
 		for items in self.parsed_json['objects']:
-			self.congress_members[items['firstname'] + ' ' + items['lastname'] ] = items['id']
+			self.congress_members[items['firstname'] + ' ' + items['lastname'] ] = items['id']	
 	
-	def getMemberID(self):
-		return self.parsed_json['objects'][0]['bioguideid']
+	def get_Member_ID(self):
+		return str(self.parsed_json['objects'][0]['id'])
 
-	def getBillID(self):
+	def get_Bill_ID(self):
 		#This queries the GovTrack Database for bills and returns the ID of that specific bills
-
-		a = "A resolution electing officers of the House of Representatives"
-		f = GovTrack.urllib2.urlopen('https://www.govtrack.us/api/v2/bill?q=' + a.replace(" ","%20"))
+		a = str(input('Whats the Bill ID: '))
+		f = GovTrack.urllib2.urlopen('https://www.govtrack.us/api/v2/bill/' + a)
 		json_string = f.read()
 		self.parsed_json = GovTrack.json.loads(json_string)
 		f.close()
-		return self.parsed_json['objects'][0]["id"]
+		print self.parsed_json['title']
+		print self.parsed_json['current_status_date']
+		return a
+
+	def search_Bill_ID(self):
+		#This queries the GovTrack Database for bills and returns the ID of that specific bills
+		a = str(raw_input('What bill are you looking for?: '))
+		f = GovTrack.urllib2.urlopen('https://www.govtrack.us/api/v2/bill?q=' + self.a.replace(" ","%20")+ '&bill_type=house_bill')
+		json_string = f.read()
+		self.parsed_json = GovTrack.json.loads(json_string)["objects"]
+		f.close()
+		self.counter = 5
+
+		for items in self.parsed_json:
+			for items in range(5):
+				print items['title'] + '--- Bill ID ---' + str(items['id']) + '--- Introduced Year ---' + str(items['introduced_date'])
+				self.counter -= 1
+				print self.counter
 
 		#You can filter out whether or non someone was in office of the time of the bill and they weren't in office, you can just not run the program.
 
-	def getVote_Voter(self):
+	def get_Vote_Voter(self):
 		#id number
-		ref_id = str(GovTrack().getMemberID())
-		f = GovTrack.urllib2.urlopen('https://www.govtrack.us/api/v2/vote_voter/'+ ref_id)
-		json_string = f.read()
-		self.parsed_json = GovTrack.json.loads(json_string)['objects'][0]['option']
-		f.close()
+		try:
+			ref_id = str(GovTrack().getMemberID())
+			f = GovTrack.urllib2.urlopen('https://www.govtrack.us/api/v2/vote_voter?person='+ GovTrack().getMemberID() +'&vote='+GovTrack().getBillID())
+			json_string = f.read()
+			self.parsed_json = GovTrack.json.loads(json_string)['objects'][0]['option']
+			print self.parsed_json
+			f.close()
 
-		return self.parsed_json.get('value')
+			return self.parsed_json.get('value')
+		except IndexError, ValueError:
+			print 'Please select someone who is supposed to be voting on this bill.'
+
+	# def getVote(self):
+	# 	f = GovTrack.urllib2.urlopen("https://www.govtrack.us/api/v2/vote/" + str((GovTrack().getMemberID())))
+	# 	json_string = f.read()
+	# 	self.parsed_json = GovTrack.json.loads(json_string)['objects']
+	# 	f.close()
+
+	# 	print parsed_json
+
+		
 		#billID
-		billID = str(GovTrack().getBillID())
-
-
-
-print GovTrack().getVote_Voter()
+#76416
+print GovTrack().search_Bill_ID()
